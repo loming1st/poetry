1a9782e7c015f60e7dbacefb35d8ef9805b6119a
loming1st <laomingming@outlook.com>
2023-07-24 17:02:20 +0800

    fix url bug

--- src/poetry/repositories/link_sources/html.py
+++ src/poetry/repositories/link_sources/html.py
@@ -56,6 +56,4 @@ class HTMLPage(LinkSource):
 
 class SimpleRepositoryPage(HTMLPage):
     def __init__(self, url: str, content: str) -> None:
-        if not url.endswith("/"):
-            url += "/"
         super().__init__(url=url, content=content)
